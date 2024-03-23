90e9bc36fa726848481164b946cf0e244f735aec
bimaassist15 <149299126+bimaassist15@users.noreply.github.com>
2024-03-23 17:14:09 +0700

    responsive app

--- resources/views/one/home/modal.blade.php
+++ resources/views/one/home/modal.blade.php
@@ -13,91 +13,95 @@
                 <h6 class="m-0">Berikut merupakan informasi detail mengenai hasil ujian kamu:</h6>
                 <p class="mt-2 text-white font-weight-bold p-2" style="border-radius: 5px; background: #378CE7;">Biodata
                 </p>
-                <table class="w-100 mb-0">
-                    <tr>
-                        <td>
-                            <table class="w-100 mb-0">
-                                <tr>
-                                    <td class="text-dark" style="width: 30%;">NISN</td>
-                                    <td class="text-dark" style="width: 1%;">:</td>
-                                    <td class="text-dark">{{ $getSiswa->nisn_siswa }}</td>
-                                </tr>
-                                <tr>
-                                    <td class="text-dark" style="width: 30%;">Nama Siswa</td>
-                                    <td class="text-dark" style="width: 1%;">:</td>
-                                    <td class="text-dark">{{ $getSiswa->nama_siswa }}</td>
-                                </tr>
-                                <tr>
-                                    <td class="text-dark" style="width: 30%;">Kelas</td>
-                                    <td class="text-dark" style="width: 1%;">:</td>
-                                    <td class="text-dark">{{ $getSiswa->kelas->nama_kelas }}</td>
-                                </tr>
+                <div class="table-responsive">
+                    <table class="w-100 mb-0">
+                        <tr>
+                            <td>
+                                <table class="w-100 mb-0">
+                                    <tr>
+                                        <td class="text-dark" style="width: 30%;">NISN</td>
+                                        <td class="text-dark" style="width: 1%;">:</td>
+                                        <td class="text-dark">{{ $getSiswa->nisn_siswa }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-dark" style="width: 30%;">Nama Siswa</td>
+                                        <td class="text-dark" style="width: 1%;">:</td>
+                                        <td class="text-dark">{{ $getSiswa->nama_siswa }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-dark" style="width: 30%;">Kelas</td>
+                                        <td class="text-dark" style="width: 1%;">:</td>
+                                        <td class="text-dark">{{ $getSiswa->kelas->nama_kelas }}</td>
+                                    </tr>
 
-                            </table>
-                        </td>
-                        <td style="vertical-align: top;">
-                            <table class="w-100 mb-0">
-                                @if ($getSiswa->jeniskelamin_siswa)
-                                    <tr>
-                                        <td class="text-dark" style="width: 30%;">Jenis kelamin</td>
-                                        <td class="text-dark" style="width: 1%;">:</td>
-                                        <td class="text-dark">{{ $getSiswa->jeniskelamin_siswa }}</td>
-                                    </tr>
-                                @endif
-                                @if ($getSiswa->email_siswa)
-                                    <tr>
-                                        <td class="text-dark" style="width: 30%;">Email</td>
-                                        <td class="text-dark" style="width: 1%;">:</td>
-                                        <td class="text-dark">{{ $getSiswa->email_siswa }}</td>
-                                    </tr>
-                                @endif
-                                @if ($getSiswa->alamat_siswa)
-                                    <tr>
-                                        <td class="text-dark" style="width: 30%;">Alamat</td>
-                                        <td class="text-dark" style="width: 1%;">:</td>
-                                        <td class="text-dark">{{ $getSiswa->alamat_siswa }}</td>
-                                    </tr>
-                                @endif
-                                @if ($getSiswa->namaorangtua_siswa)
-                                    <tr>
-                                        <td class="text-dark" style="width: 30%;">Orang Tua</td>
-                                        <td class="text-dark" style="width: 1%;">:</td>
-                                        <td class="text-dark">{{ $getSiswa->namaorangtua_siswa }}</td>
-                                    </tr>
-                                @endif
-                            </table>
-                        </td>
-                    </tr>
-                </table>
+                                </table>
+                            </td>
+                            <td style="vertical-align: top;">
+                                <table class="w-100 mb-0">
+                                    @if ($getSiswa->jeniskelamin_siswa)
+                                        <tr>
+                                            <td class="text-dark" style="width: 30%;">Jenis kelamin</td>
+                                            <td class="text-dark" style="width: 1%;">:</td>
+                                            <td class="text-dark">{{ $getSiswa->jeniskelamin_siswa }}</td>
+                                        </tr>
+                                    @endif
+                                    @if ($getSiswa->email_siswa)
+                                        <tr>
+                                            <td class="text-dark" style="width: 30%;">Email</td>
+                                            <td class="text-dark" style="width: 1%;">:</td>
+                                            <td class="text-dark">{{ $getSiswa->email_siswa }}</td>
+                                        </tr>
+                                    @endif
+                                    @if ($getSiswa->alamat_siswa)
+                                        <tr>
+                                            <td class="text-dark" style="width: 30%;">Alamat</td>
+                                            <td class="text-dark" style="width: 1%;">:</td>
+                                            <td class="text-dark">{{ $getSiswa->alamat_siswa }}</td>
+                                        </tr>
+                                    @endif
+                                    @if ($getSiswa->namaorangtua_siswa)
+                                        <tr>
+                                            <td class="text-dark" style="width: 30%;">Orang Tua</td>
+                                            <td class="text-dark" style="width: 1%;">:</td>
+                                            <td class="text-dark">{{ $getSiswa->namaorangtua_siswa }}</td>
+                                        </tr>
+                                    @endif
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
             </div>
             <div class="mt-3">
                 <p class="text-white font-weight-bold p-2" style="border-radius: 5px; background: #378CE7;">Hasil Nilai
                     Ujian</p>
                 <span class="text-dark">Klik Link pada kolom hasil ujian untuk melihat rincian nilai kamu.</span>
-                <table class="table mt-1">
-                    <thead>
-                        <tr>
-                            <th>No.</th>
-                            <th>Hasil Ujian</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @php
-                            $no = 1;
-                        @endphp
-                        @foreach ($getSiswa->nilaiSiswa as $item)
+                <div class="table-responsive">
+                    <table class="table mt-1">
+                        <thead>
                             <tr>
-                                <td>{{ $no++ }}</td>
-                                <td>
-                                    <a href="{{ $item->nilai_nsiswa }}" class="text-primary font-weight-bold"
-                                        target="_blank">
-                                        {{ $item->nilai_nsiswa }}
-                                    </a>
-                                </td>
+                                <th>No.</th>
+                                <th>Hasil Ujian</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @php
+                                $no = 1;
+                            @endphp
+                            @foreach ($getSiswa->nilaiSiswa as $item)
+                                <tr>
+                                    <td>{{ $no++ }}</td>
+                                    <td>
+                                        <a href="{{ $item->nilai_nsiswa }}" class="text-primary font-weight-bold"
+                                            target="_blank">
+                                            {{ $item->nilai_nsiswa }}
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
         <div class="modal-footer">
